3cd69dd50e87b018dc2ac93af4574e29347d3f6b
Dave Greenwood <dgrnwd@gmail.com>
2019-07-08 18:30:42 +0100

    remove unused imports and commented code

--- drender/render.py
+++ drender/render.py
@@ -1,10 +1,5 @@
 """Render an object"""
 import torch
-import time
-import numpy as np
-from PIL import Image
-from drender.utils import Rcube
-import matplotlib.pyplot as plt
 
 
 def inside_outside(pixels, triangles):
@@ -42,32 +37,3 @@ def inside_outside(pixels, triangles):
         * torch.clamp(t3, min=0.)
 
     return barycentrics, mask
-
-# -----------------------------------------------------------------------------
-# TESTING
-# -----------------------------------------------------------------------------
-
-if __name__ == "__main__":
-    size = 256
-    rcube = Rcube(size)
-    tris, uvs = rcube.tris()
-    xx, yy = torch.meshgrid([torch.arange(0, size), torch.arange(0, size)])
-    pixels = torch.stack([xx, yy], dim=-1).reshape(-1, 2)
-
-    t0 = time.time()
-    barycentrics, mask = inside_outside(pixels, tris[:, :, :2])
-    t1 = time.time()
-    print(f"time: {t1-t0:0.2f}")
-
-    # image out
-    img = np.zeros([size, size])
-    img[mask.sum(0).reshape(size, size).numpy() > 0] = 1.0
-    Image.fromarray(img * 255).convert("RGB").save("rcube.jpg")
-
-
-# class Raster(object):
-#     """"""
-
-#     def __init__(self, size: int = 256):
-#         super(Raster, self).__init__()
-
