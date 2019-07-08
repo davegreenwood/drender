988b95c5706d8316d6b9fe0174dba6b3ccaa7bda
Dave Greenwood <dgrnwd@gmail.com>
2019-07-08 19:18:40 +0100

    proper device setting

--- examples/rcube.py
+++ examples/rcube.py
@@ -15,9 +15,14 @@ if __name__ == "__main__":
     size = 256
     rcube = Rcube(size)
     tris, uvs = rcube.tris()
-    xx, yy = torch.meshgrid([torch.arange(0, size), torch.arange(0, size)])
+
+    xx, yy = torch.meshgrid([
+        torch.arange(0, size).to(device),
+        torch.arange(0, size).to(device)])
     pixels = torch.stack([xx, yy], dim=-1).reshape(-1, 2)
-    pixels.to(device)
+
+    print(tris.device)
+    print(pixels.device)
 
     t0 = time.time()
     barycentrics, mask = inside_outside(pixels, tris[:, :, :2])
@@ -26,5 +31,5 @@ if __name__ == "__main__":
 
     # image out
     img = np.zeros([size, size])
-    img[mask.sum(0).reshape(size, size).numpy() > 0] = 1.0
+    img[mask.sum(0).reshape(size, size).cpu().numpy() > 0] = 1.0
     Image.fromarray(img * 255).convert("RGB").save("rcube.jpg")
