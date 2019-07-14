ba27a7fee0d680eecdb6fec835c158c74f85ab32
Dave Greenwood <david.greenwood@uea.ac.uk>
2019-07-14 10:26:42 +0100

    big changes - it's research code :)

--- drender/render.py
+++ drender/render.py
@@ -1,39 +1,179 @@
 """Render an object"""
 import torch
+import numpy as np
+
+DTYPE = torch.float
+DEVICE = "cpu"
 
 
-def inside_outside(pixels, triangles):
+def backface_cull(tris):
     """
-    pixels: ndarray [npixels, 2(x, y)]
-    triangles: ndarray [ntris, 3, 2(x, y)]
+    Return only the triangles that face forward. Tris are assumed to be
+    in view space."""
+    normals = torch.cross(tris[:, 1] - tris[:, 0], tris[:, 2] - tris[:, 0])
+    m = torch.tensor([0, 0, 1.0], dtype=DTYPE, device=DEVICE) @ normals
+    return tris[m > 0]
+
+
+def aabb(tri):
     """
-    pixels_f = pixels.float()
+    Tri is tensor in screen space - screen space is 2d in -1, 1.
+    returns axis aligned box clamped to screen space.
+    tri : tensor 3 x 2 (3 points by (x, y))
+    TODO: What if min and max are equal ie. the tri is off screen...
+    """
+    _min, _max = tri.min(dim=0)[0], tri.max(dim=0)[0]
+    return torch.cat(
+        [torch.clamp(_min, min=-1, max=1),
+         torch.clamp(_max, min=-1, max=1)])
 
-    edge_0 = triangles[:, 1] - triangles[:, 0]
-    edge_1 = triangles[:, 2] - triangles[:, 1]
-    edge_2 = triangles[:, 0] - triangles[:, 2]
 
-    norm = edge_0[:, 0] * edge_2[:, 1] - edge_0[:, 1] * edge_2[:, 0] + 1e-8
-    norm = torch.unsqueeze(norm, dim=-1)
+def box2pts(bb, size):
+    """
+    return (x, y) coordinates within a bounding box.
+    NOTE: This is non differentiable - does it matter?
+    """
+    bb += 1.0
+    bb /= 2
+    bb *= size
+    x1, y1, x2, y2 = bb.long()
+    xx, yy = torch.meshgrid(
+        [torch.arange(x1, x2), torch.arange(y1, y2)])
+    return torch.stack([xx, yy], dim=-1).reshape(2, -1)
 
-    # vectors from vertices to the point
-    p0 = triangles[:, None, 0] - pixels_f
-    p1 = triangles[:, None, 1] - pixels_f
-    p2 = triangles[:, None, 2] - pixels_f
 
-    t1 = (edge_0[:, 0, None] * p0[:, :, 1] -
-          edge_0[:, 1, None] * p0[:, :, 0]) * norm
-    t2 = (edge_1[:, 0, None] * p1[:, :, 1] -
-          edge_1[:, 1, None] * p1[:, :, 0]) * norm
-    t3 = (edge_2[:, 0, None] * p2[:, :, 1] -
-          edge_2[:, 1, None] * p2[:, :, 0]) * norm
+def area2d(a, b, c):
+    """
+    Vectorised area of 2d parallelogram (divide by 2 for triangle)
+    (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)
+    NB: colinear points result in zero area.
+    """
+    return (b[0] - a[:, 0]) * (c[1] - a[:, 1]) - \
+        (b[1] - a[:, 1]) * (c[0] - a[:, 0])
 
-    bx = t2 / t1
-    by = t3 / t1
+
+def barys(pAB, pBC, pCA):
+    """sub triangle areas to barycentric
+    TODO: potential division by zero here.
+    """
+    bx = pBC / pAB
+    by = pCA / pAB
     bz = 1.0 - bx - by
-    barycentrics = torch.stack([bx, by, bz], dim=-1)
+    return torch.cat([bx[..., None], by[..., None], bz[..., None]], dim=-1)
 
-    mask = torch.clamp(t1, min=0.) * torch.clamp(t2, min=0.) \
-        * torch.clamp(t3, min=0.)
 
-    return barycentrics, mask
+def inside_mask(w1, w2, w3):
+    """
+    From weighted triangles, decide if all are greater than zero.
+    returns a mask: [1, 1, 0, ... 1, 0]
+    NOTE: is this operation out of graph as non - differentiable?
+    """
+    stacked = torch.stack([w1, w2, w3])
+    clamped = torch.clamp(stacked, min=0)
+    product = torch.prod(clamped, dim=0)
+    m = torch.zeros_like(product, dtype=torch.uint8, device=DEVICE)
+    m[product > 0] = 1
+    return m
+
+
+def assert_size(x, y):
+    """Make sure x and y are equal powers of 2 """
+    assert x == y
+    assert x in [8, 16, 32, 64, 128, 256, 512, 1024]
+
+
+def lookup_table(size, dtype=DTYPE, device=DEVICE):
+    """return a square table (size x size), of 2d points (x, y) in -1, 1"""
+    xx, yy = torch.meshgrid(
+        [torch.linspace(-1.0, 1.0, size, dtype=dtype, device=device),
+         torch.linspace(-1.0, 1.0, size, dtype=dtype, device=device)])
+    return torch.stack([xx, yy], dim=-1)
+
+
+class Render(torch.nn.Module):
+    """Render trangles in view space (-1, 1,)"""
+
+    def __init__(self, size):
+        super(Render, self).__init__()
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        dtype = torch.float
+        self.pts = None
+        self.result = None
+        self.zmin = 0.0
+        self.size = size
+        self.dtype = dtype
+        self.device = device
+
+    def forward(self, tris):
+        super(Render, self).forward()
+        return self.render(tris)
+
+    def render(self, tris):
+        """do render """
+        zmin = tris.view(-1).view(-1, 3).min(0)[0][-1]
+        self.result = torch.zeros(
+            [self.size, self.size, 4], dtype=self.dtype, device=self.device)
+        self.zbuffer = zmin * torch.ones(
+            [self.size, self.size, 1], dtype=self.dtype, device=self.device)
+        self.pts = lookup_table(
+            self.size, dtype=self.dtype, device=self.device)
+        for t in tris:
+            self.raster(t)
+        self.zbuffer -= zmin
+        self.zbuffer /= self.zbuffer.view(-1).max(0)[0]
+
+    def aabb_idx(self, tri):
+        """
+        Return the (x, y) indices of points within the triangle AABB.
+        If, the box is empty the coords will be None, None
+        """
+        bb = aabb(tri)
+        x, y = box2pts(bb, self.size)
+        return x, y
+
+    def raster(self, tri):
+        """
+        triangle tri, iff the point is in the triangle.
+        tri : tensor 3 x 2 (3 points by (x, y))
+        """
+
+        # mostly 2d - assuming the triangle is in view space
+        tri2d = tri[:, :2]
+
+        # index to everything
+        x, y = self.aabb_idx(tri2d)
+        if x.shape[0] == 0:
+            return None
+
+        # area of tri
+        w = area2d(tri2d[None, 0], tri2d[1], tri2d[2])
+        if torch.isclose(w, torch.zeros_like(w)):
+            return None
+
+        # signed weighted area of subtriangles, NB: any could be zero
+        pts = self.pts[x, y, :]
+        pAB = area2d(pts, tri2d[0], tri2d[1]) * w
+        pBC = area2d(pts, tri2d[1], tri2d[2]) * w
+        pCA = area2d(pts, tri2d[2], tri2d[0]) * w
+
+        # mask for pts that are in the triangle,
+        pts_msk = inside_mask(pAB, pBC, pCA)
+        if torch.allclose(pts_msk, torch.zeros_like(pts_msk)):
+            return None
+
+        # interpolated 3d points to consider for render
+        pts3d = barys(pAB, pBC, pCA) @ tri
+
+        # keep points that are nearer than existing zbuffer
+        zbuffer = self.zbuffer[x, y, :].view(-1)
+        zbf_msk = pts3d[:, 2].view(-1) >= zbuffer
+        if torch.allclose(zbf_msk, torch.zeros_like(zbf_msk)):
+            return None
+
+        # render points nearer and in triangle
+        rnd_msk = pts_msk * zbf_msk
+
+        self.zbuffer[x[rnd_msk], y[rnd_msk], :] = pts3d[rnd_msk, 2, None]
+        self.result[x[rnd_msk], y[rnd_msk], :3] = pts3d[rnd_msk, :3]
+        self.result[x[rnd_msk], y[rnd_msk], 3] = 1.0
+        return None
--- drender/utils.py
+++ drender/utils.py
@@ -8,6 +8,8 @@ from PIL import Image
 
 RCUBE = resource_filename(__name__, "data/rcube.obj")
 UVMAP = resource_filename(__name__, "data/util-mark6.png")
+DTYPE = torch.float
+DEVICE = "cpu"
 
 
 def read_obj(fname):
@@ -37,30 +39,87 @@ def read_obj(fname):
 class Rcube:
     """test object - a cube with the vertices rotated."""
 
-    def __init__(self, size: int = 256):
+    def __init__(self):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         (v, f, uv, uvf), t = read_obj(RCUBE), ToTensor()
         self.v = torch.from_numpy(v).to(device)
         self.uv = torch.from_numpy(uv).to(device)
-        self.map = t(Image.open(UVMAP).convert("RGB")).to(device)
+        self.uvmap = t(Image.open(UVMAP).convert("RGB")).to(device)
         self.f = torch.from_numpy(f).to(device)
         self.uvf = torch.from_numpy(uvf).to(device)
-        self.size = size
         self.device = device
 
     def tris(self):
-        """Triangles scaled to fit a square image of (size * size).
+        """Triangles as tensors.
         returns tuple: (vertex_triangles, uv_triangles)
         """
-        _tris = self.v[self.f]
-        _tris += torch.tensor([1.0, 1.0, 0.0], device=self.device)
-        _tris *= self.size / 2.0
-        return _tris, self.uv[self.uvf]
+        return self.v[self.f], self.uv[self.uvf]
 
     def uvmap(self):
         """Return the uvmap image as PIL image """
         t = ToPILImage()
-        return t(self.map)
+        return t(self.uvmap)
+
+
+def area2d(a, b, c):
+    """
+    Vectorised area of 2d parallelogram (divide by 2 for triangle)
+    (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)
+    NB: colinear points result in zero area.
+    """
+    return (b[0] - a[:, 0]) * (c[1] - a[:, 1]) - \
+        (b[1] - a[:, 1]) * (c[0] - a[:, 0])
+
+
+def mask(w1, w2, w3):
+    """
+    From weighted triangles, decide if all are greater than zero.
+    NOTE: is this operation out of graph as non - differentiable?
+    """
+    stacked = torch.stack([w1, w2, w3])
+    clamped = torch.clamp(stacked, min=0)
+    product = torch.prod(clamped, dim=0)
+    _mask = torch.zeros_like(product, dtype=torch.long, device=DEVICE)
+    _mask[product > 0] = 1
+    return _mask
+
+
+def pts_in_tri(pts, tri):
+    """
+    Return the barycentric coords of each point in pts for the triangle tri,
+    iff the point is in the triangle.
+    tri : tensor 3 x 2 (3 points by (x, y))
+    pts: tensor n x 2 (n points by (x, y))
+
+    return
+        tensor: m x 5 (m x [x, y, b1, b2, b3]), where m is the number of points
+        that were found to be in the triangle. If no points
+    """
+
+    # area of tri
+    w = area2d(tri[None, 0], tri[1], tri[2])
+
+    if torch.isclose(w, torch.zeros_like(w)):
+        # print("Triangle is degenerate")
+        return None
+
+    # signed weighted area of subtriangles, NB: any could be zero
+    pAB = area2d(pts, tri[0], tri[1]) * w
+    pBC = area2d(pts, tri[1], tri[2]) * w
+    pCA = area2d(pts, tri[2], tri[0]) * w
+
+    # mask for pts that are in the triangle, if zero don't calc barycentrics.
+    m = mask(pAB, pBC, pCA)
+
+    if torch.allclose(m, torch.zeros_like(m)):
+        # print("No points in triangle.")
+        return None
+
+    bx = pBC[m > 0] / pAB[m > 0]
+    by = pCA[m > 0] / pAB[m > 0]
+    bz = 1.0 - bx - by
+    return torch.cat(
+        [pts[m > 0], bx[..., None], by[..., None], bz[..., None]], dim=1)
 
 
 if __name__ == "__main__":
