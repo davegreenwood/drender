78882f25c86598652faf6add4c9e832660f2c2c7
Dave Greenwood <david.greenwood@uea.ac.uk>
2019-07-17 16:16:06 +0100

    delete AABB autograd function

--- drender/render.py
+++ drender/render.py
@@ -5,45 +5,6 @@ DTYPE = torch.float
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-class AABB(torch.autograd.Function):
-    """
-    Return indexing values for a bounding box to be used:
-    xmin, ymin, xmax, ymax = aabb
-    slice = array2d[xmin:xmax, ymin:ymax]
-    """
-
-    @staticmethod
-    def forward(ctx, aabb, size):
-        """
-        aabb is a tensor we want to clamp to -1, 1 then scale to screen space.
-        """
-        aabb += 1.0
-        aabb /= 2
-        aabb *= size
-        aabb += torch.tensor((0., 0., 1., 1.), dtype=DTYPE, device=DEVICE)
-        aabb = torch.clamp(aabb.detach(), min=0, max=size)
-
-        ctx.save_for_backward(aabb)
-        ctx.save_for_backward(size)
-        aabb = aabb.long()
-        aabb.to(device=DEVICE)
-        return aabb
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        """
-        backward pass.
-        """
-        aabb, size = ctx.saved_tensors
-        grad_input = grad_output.clone()
-
-        grad_input[aabb < 0] = 0
-        grad_input[aabb > size] = size
-        grad_input /= size
-        grad_input *= 2
-        return grad_input, None
-
-
 def area2d(a, b, c):
     """
     Vectorised area of 2d parallelogram (divide by 2 for triangle)
